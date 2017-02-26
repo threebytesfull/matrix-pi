ec99d77621e576bf96eb68948861df31af3b8c77
Rufus Cable <rufus@threebytesfull.com>
2017-02-26 16:12:20 +0000

    Auto-detect and support multiple boards in web interface

--- the_matrix/web.py
+++ the_matrix/web.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 from .the_matrix import TheMatrix, DEFAULT_CURRENT_SOURCE_MA
 from .layout import Layout
+from .detect import detect
 
 import re
 
 app = Flask(__name__)
 
-matrix = TheMatrix()
+addresses = detect()
+matrices = dict((address, TheMatrix(address)) for address in addresses)
 
 cs_pairs = [(anode, cathode) for cathode in range(12) for anode in [a for a in range(12) if a != cathode][:10]]
 chip = [{'label': label} for label in 'GND CS6 VDD CS7 CS8 CS9 GND CS11 CS10 VDD IRQ SYNC RSTN GND SCL SDA ADDR VDD VDD CS5 CS4 GND CS2 CS3 CS0 VDD CS1 GND'.split()]
@@ -23,7 +25,10 @@ for pin in chip:
         pin['cathode_leds'] = [[int(i/5), i%5] for i in cathode_led_indices]
 
 def updateFrame(frameNumber):
-    matrix.writeOnOffFrame(frameNumber, onOffFrame)
+    for address in matrices.keys():
+        matrix = matrices[address]
+        matrix.writeOnOffFrame(frameNumber, onOffFrame)
+
     return ""
 
 @app.route('/')
@@ -43,29 +48,37 @@ def reset():
 
     isReversed = False
 
-    matrix.reset()
-    matrix.selectMemoryConfig(1)
+    for address in matrices.keys():
+        matrix = matrices[address]
 
-    ledCurrent = DEFAULT_CURRENT_SOURCE_MA
-    matrix.setCurrentSource(ledCurrent)
+        matrix.reset()
+        matrix.selectMemoryConfig(1)
 
-    layout = Layout(reversed=isReversed)
-    blinkPWMFrame = TheMatrix.BlinkPWMFrame(layout=layout)
-    matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
+        ledCurrent = DEFAULT_CURRENT_SOURCE_MA
+        matrix.setCurrentSource(ledCurrent)
 
-    onOffFrame = TheMatrix.OnOffFrame(layout=layout)
-    matrix.writeOnOffFrame(0, onOffFrame)
+        layout = Layout(reversed=isReversed)
+        blinkPWMFrame = TheMatrix.BlinkPWMFrame(layout=layout)
+        matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
 
-    matrix.setDisplayOptions()
-    matrix.display(1)
-    matrix.displayPictureFrame(0)
+        onOffFrame = TheMatrix.OnOffFrame(layout=layout)
+        matrix.writeOnOffFrame(0, onOffFrame)
+
+        matrix.setDisplayOptions()
+        matrix.display(1)
+        matrix.displayPictureFrame(0)
+
     return ""
 
 @app.route('/setCurrent/<current>')
 def setCurrent(current):
     global ledCurrent
     ledCurrent = int(current)
-    matrix.setCurrentSource(ledCurrent)
+
+    for address in matrices.keys():
+        matrix = matrices[address]
+        matrix.setCurrentSource(ledCurrent)
+
     return ""
 
 @app.route('/allOff')
@@ -74,10 +87,13 @@ def allOff():
     global onOffFrame
 
     blinkPWMFrame = TheMatrix.BlinkPWMFrame()
-    matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
-
     onOffFrame = TheMatrix.OnOffFrame(0)
-    matrix.writeOnOffFrame(0, onOffFrame)
+
+    for address in matrices.keys():
+        matrix = matrices[address]
+        matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
+        matrix.writeOnOffFrame(0, onOffFrame)
+
     return ""
 
 @app.route('/allOn')
@@ -86,10 +102,13 @@ def allOn():
     global onOffFrame
 
     blinkPWMFrame = TheMatrix.BlinkPWMFrame()
-    matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
-
     onOffFrame = TheMatrix.OnOffFrame(1)
-    matrix.writeOnOffFrame(0, onOffFrame)
+
+    for address in matrices.keys():
+        matrix = matrices[address]
+        matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
+        matrix.writeOnOffFrame(0, onOffFrame)
+
     return ""
 
 @app.route('/setPixel', methods=['POST'])
@@ -117,8 +136,12 @@ def setReversed(reversedFlag):
         newLayout = Layout(reversed=newReversed)
         blinkPWMFrame.layout = newLayout
         onOffFrame.layout = newLayout
-        matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
-        matrix.writeOnOffFrame(0, onOffFrame)
+
+        for address in matrices.keys():
+            matrix = matrices[address]
+            matrix.writeBlinkPWMFrame(0, blinkPWMFrame)
+            matrix.writeOnOffFrame(0, onOffFrame)
+
     return ""
 
 def main():
